0773e4daff2f69881b9c23830a3b6c982f0140cd
Dowen Robinson <drobinson@ad.accelclaims.com>
2020-10-10 23:41:54 -0500

    Added cross origin support

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 8080
+
+app.use(cors())
 
 const commentData = {
     1: {
